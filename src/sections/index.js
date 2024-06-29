411f0e6868adad09707349e15c613f8b35d7511f
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-06-29 16:54:55 +0200

    Creazione di tutti i componenti

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export { Hero, PopularProducts, SuperQuality, SpecialOffer, Services, CustomerReviews, Subscribe, Footer }
